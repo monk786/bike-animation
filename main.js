7d89459ec01c6aabe72c0379fd12f60fd3da3b61
Abhijeet Kumar <abhijeetkumar_1992@yahoo.in>
2018-04-13 01:43:40 +0530

    Animations

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 (function() {
-    /**
-     * Creates a WURFL object globally, of the structure
-     * {
-     *      is_mobile: <boolean>,
-     *      complete_device_name: <String>,
-     *      form_factor: <String>
-     * }
-     */
-    eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('8 7={"6":5,"4":"3 2","1":"0"};',9,9,'Desktop|form_factor|Chrome|Google|complete_device_name|false|is_mobile|WURFL|var'.split('|'),0,{}))
 
     var bgImage = new Image(),
         bikeAtlas = new Image(),
-        startPos = 0,
-        vw = "100%",
-        vh = window.innerHeight,
-        bgImageSrc = "",
-        terrain = document.getElementById("game");
+        vh = window.innerHeight;
+
+    var Platform = {
+        mobile: window.matchMedia("(max-width: 768px)"),
+        tablet: window.matchMedia("(min-width: 768px) and (max-width: 1024px)"),
+        desktop: window.matchMedia("(min-width: 1024px) and (max-width: 1440px)"),
+        largeDesktop: window.matchMedia("(min-width: 1440px")
+    };
 
     var Util = {
         getBackgroundImage: function() {
-            var platform = WURFL.form_factor.toLowerCase();
-            
+            var platform = "desktop";
+
+            for ( var pltfrm in Platform ) {
+                if ( Platform[pltfrm].matches ) {
+                    platform = pltfrm;
+                    break;
+                }
+            }
+
             return Terrain.images[platform];
         },
 
@@ -31,11 +32,6 @@
 
     var Bike = {
         image: "./assets/images/bikes.png",
-
-        setStartPosition: function() {
-            // return [ Bike.positions[0][0] * bgImage.clientWidth * Math.abs(Terrain.scale), vh - ( Bike.positions[0][1] * bgImage.clientHeight * Math.abs(Terrain.scale) ) ];
-            return [ Bike.positions[0][0] * bgImage.clientWidth, vh - ( Bike.positions[0][1] * bgImage.clientHeight ) ];
-        },
 
         position: [0, 0],
 
@@ -82,6 +78,10 @@
 
         },
 
+        getStartPosition: function() {
+            return [ Bike.positions[0][0] * bgImage.clientWidth, Bike.positions[0][1] * bgImage.clientHeight ];
+        },
+
         setToNormal: function() {
             var bike = Bike.container;
             Bike.orientation = "normal";
@@ -90,37 +90,36 @@
         },
 
         sway: function() {
-            var bike = Bike.container;
+            var bikeContainer = Bike.container;
 
-            bike.style.background = "url(" + Bike.image + ") " + Bike.rider[Bike.selectedRider].image[Bike.orientation][0] + "px " + Bike.rider[Bike.selectedRider].image[Bike.orientation][1] + "px";
-            bike.style.width = "121px";
-            bike.style.height = "188px";
+            bikeContainer.style.background = "url(" + Bike.image + ") " + Bike.rider[Bike.selectedRider].image[Bike.orientation][0] + "px " + Bike.rider[Bike.selectedRider].image[Bike.orientation][1] + "px";
+            bikeContainer.style.width = "121px";
+            bikeContainer.style.height = "188px";
             Bike.turn();
         },
 
         turn: function() {
-            console.log(Bike.position);
-            Bike.position = [ ( Bike.orientation === "left" ? Bike.position[0] - (0.05 * bgImage.clientWidth) : Bike.position[0] + (0.05 * bgImage.clientWidth) ), Bike.position[1]];
-            console.log(Bike.position);
-            TweenMax.to(Bike.container, 0.5, { css: { transform: "translate("+ Bike.position[0] +"px, " + Bike.position[1] + "px)" }, onComplete: Bike.setToNormal });
+            Bike.position = [ ( Bike.orientation === "left" ? -(0.05 * bgImage.clientWidth) : (0.05 * bgImage.clientWidth) ), 0];
+            TweenMax.to(Bike.container, 0.5, { css: { transform: "scale(" + bgImage.clientHeight / 25083 + ") translate("+ Bike.position[0] +"px, " + Bike.position[1] + "px)" }, onComplete: Bike.setToNormal });
         },
 
-        firstLevel: function() {
-            var startPos = Bike.setStartPosition();
+        renderOnStartPosition: function() {
+            var startPos = Bike.getStartPosition();
             Bike.position = [startPos[0] - Bike.dimensions.normal.cx, startPos[1] - Bike.dimensions.normal.cy];
             var bike = Bike.container;
 
             bike.style.background = "url(" + Bike.image + ") " + Bike.rider[Bike.selectedRider].image[Bike.orientation][0] + "px " + Bike.rider[Bike.selectedRider].image[Bike.orientation][1] + "px";
-            bike.style.transform = "translate("+ Bike.position[0] +"px, " + Bike.position[1] + "px)";
+            bike.style.bottom = Bike.position[1] + "px";
             bike.className = "bike";
             bike.style.width = "121px";
             bike.style.height = "188px";
+            bike.style.transform = "scale(" + bgImage.clientHeight / 25083 + ")";
             Game.arena.appendChild(bike);
         },
 
         render: function() {
             if ( Game.background ) {
-                Bike.firstLevel();
+                Bike.renderOnStartPosition();
             } else {
                 setTimeout(Bike.render, 100);
             }
@@ -129,12 +128,15 @@
 
     var Terrain = {
         images: {
+            "largeDesktop": "./assets/images/race_background_large_desktop.png",
             "desktop": "./assets/images/race_background_desktop.png",
             "mobile": "./assets/images/race_background_mobile.png",
-            "smartphone": "./assets/images/race_background_tablet.png"
+            "tablet": "./assets/images/race_background_tablet.png"
         },
 
-        // easeAnimation: CustomEase.create("custom", "M0,0,C0.266,0.412,0.453,0.831,0.582,0.952,0.626,0.993,0.78,1,1,1"),
+        // easeAnimation: CustomEase.create("custom", "M0,0 C0.017,0 0.034,0.017 0.051,0.018 0.092,0.019 0.168,0.063 0.21,0.05 0.281,0.059 0.297,0.105 0.349,0.124 0.434,0.154 0.498,0.224 0.55,0.248 0.613,0.277 0.617,0.388 0.662,0.416 0.715,0.449 0.745,0.497 0.796,0.544 0.856,0.599 0.936,0.673 1,1"),
+
+        easeAnimation: Power1.easeIn,
 
         step: 0,
 
@@ -149,7 +151,7 @@
             Terrain.scale = Util.getScale(bgImage.naturalHeight, bgImage.clientHeight);
             Terrain.step = bgImage.clientHeight / 10;
 
-            startPos = -( bgImage.height - vh );
+            var startPos = -( bgImage.height - vh );
             Terrain.position = startPos;
             bgImage.style.transform = "translate(0px, " + startPos + "px)";
             setTimeout(Game.run, 2000);
@@ -179,18 +181,18 @@
             var yPos = -(bgImage.clientHeight - Terrain.step);
             Terrain.position = yPos;
             Game.level++;
-            TweenMax.to(bgImage, 2, { ease: Power4.easeOut, css: { transform: "translate(0px, " + yPos + "px)" }, onComplete: Game.levelUp });
+            TweenMax.to(bgImage, 2, { ease: Terrain.easeAnimation, css: { transform: "translate(0px, " + yPos + "px)" } });
         },
 
         levelUp: function() {
             var yPos = Terrain.position = -(Math.abs(Terrain.position) - Terrain.step);
             Game.level++;
             Bike.orientation = Game.level % 2 === 0 ? "left" : "right"; // TODO: random or according to path
-            Bike.sway();
+            setTimeout(Bike.sway, 2000);
             if (Game.level === 10) {
-                TweenMax.to(bgImage, 4, { ease: Power4.easeOut, css: { transform: "translate(0px, " + yPos + "px)" }, onComplete: Game.complete });    
+                TweenMax.to(bgImage, 4, { ease: Terrain.easeAnimation, css: { transform: "translate(0px, " + yPos + "px)" }, onComplete: Game.complete });    
             } else {
-                TweenMax.to(bgImage, 4, { ease: Power4.easeOut, css: { transform: "translate(0px, " + yPos + "px)" }, onComplete: Game.levelUp });
+                TweenMax.to(bgImage, 4, { ease: Terrain.easeAnimation, css: { transform: "translate(0px, " + yPos + "px)" } });
             }
         },
 
@@ -212,5 +214,10 @@
     };
     bikeAtlas.className = "bike";
     bikeAtlas.src = Bike.image;
+
+    /** TODO: Remove these lines */
+    window.Game = Game;
+    window.Bike = Bike;
+    window.Terrain = Terrain;
 
 })();
